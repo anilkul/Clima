12a92a60e5026a7a61180d51adc0a3fa777da36b
anilkul <mehmetanilkul@gmail.com>
2018-06-17 17:19:03 +0300

    first commit

---
+++ Clima/WeatherViewController.swift
@@ -0,0 +1,176 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Mehmet Anıl Kul on 9.08.2017.
+//  Copyright © 2017 Mehmet Anıl Kul. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
+
+
+class WeatherViewController: UIViewController, CLLocationManagerDelegate, ChangeCityDelegate {
+    
+    
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "a6c7377af4f8db681c5d56ee69b60452"
+    
+
+    //TODO: Declare instance variables here
+    let locationManager = CLLocationManager()
+    let weatherDataModel = WeatherDataModel()
+    
+    //Pre-linked IBOutlets
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        //TODO:Set up the location manager here.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+
+    }
+    
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    //Write the getWeatherData method here:
+    func getWeatherData(url: String, parameters: [String : String]) {
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON { (response) in
+            if response.result.isSuccess {
+                print("Succes! Got the weather data")
+                // response.result succcesful olacagi icin response.result.value force unwrap edilebilir
+                let weatherJSON : JSON = JSON(response.result.value!)
+                self.updateWeatherData(json: weatherJSON)
+                print(weatherJSON)
+            } else {
+                print("Error: \(response.result.error)")
+                self.cityLabel.text = "Connection Issues"
+            }
+            
+        }
+        
+        
+    }
+
+    
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+   
+    
+    //Write the updateWeatherData method here:
+    func updateWeatherData(json: JSON) {
+        // temp degisken bir deger bu nedenle optional. Bu yuzde optional binding yaptik
+        if let tempResult = json["main"]["temp"].double {
+            weatherDataModel.temperature = Int(tempResult - 273.15)
+            weatherDataModel.city = json["name"].stringValue
+            weatherDataModel.condition = json["weather"][0]["id"].intValue
+            weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+            updateUIWithWeatherData()
+        }
+        
+//        else {
+//            print("Cannot get temperature value")
+//            cityLabel.text = "Weather Unavailable"
+//        }
+  
+    }
+
+    
+    
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    
+    
+    //Write the updateUIWithWeatherData method here:
+    func updateUIWithWeatherData() {
+        cityLabel.text = weatherDataModel.city
+        temperatureLabel.text = "\(weatherDataModel.temperature)"
+        weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
+    }
+    
+    
+    
+    
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+    
+    
+    //Write the didUpdateLocations method here:
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            // location.horizontalAccuracy < 0 olursa hata aliriz bu yuzden kodu guvence altina alalim
+            if location.horizontalAccuracy > 0 {
+                // sonuc alir almaz update islemini durduralim
+                locationManager.stopUpdatingLocation()
+            }
+            print("latitude: \(location.coordinate.latitude) longitude: \(location.coordinate.longitude)")
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params: [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+            getWeatherData(url: WEATHER_URL, parameters: params)
+        }
+       
+    }
+    
+    
+    //Write the didFailWithError method here:
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
+    
+    
+
+    
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+    
+    
+    //Write the userEnteredANewCityName Delegate method here:
+    func userEnteredANewCityName(city: String) {
+        let params : [String : String] = ["q" : city, "appid" : APP_ID]
+        getWeatherData(url: WEATHER_URL, parameters: params)
+        
+        
+    }
+
+    
+    //Write the PrepareForSegue Method here
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "changeCityName" {
+            // destination segue'miz bir ChangeCityViewController olacak
+            if let destinationVC = segue.destination as? ChangeCityViewController {
+                destinationVC.delegate = self
+            }
+            
+        }
+    }
+    
+    
+    
+    
+}
+
+
